d54417b115f9c23ec5f12bd9d5c92257564d7a16
annerob01 <anne-marie.robert@groupecanam.com>
2025-06-16 15:59:40 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 #session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.search_on").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 
