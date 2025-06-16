b9c0a4b58bfa6cdd6d8be3796d3b33d22a3e2d1f
annerob01 <anne-marie.robert@groupecanam.com>
2025-06-16 15:44:30 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string+= fruit_chosen + ' '
-        st.subheader(fruit_chosen + ' :' + str(fruit_chosen) + ': ' + 'Nutrition Infos')
+        st.subheader(fruit_chosen + ' :' + str(fruit_chosen).lower() + ': ' + 'Nutrition Infos')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
       
