0f1ac3b5abecb6bca8be68565103475ad1ca12df
annerob01 <anne-marie.robert@groupecanam.com>
2025-06-16 16:22:11 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ my_dataframe = session.table("smoothies.public.FRUIT_OPTIONS").select(col('FRUIT
 
 pd_df = my_dataframe.to_pandas()
 st.dataframe(pd_df)
-st.stop()
+#st.stop()
 
 
 ingredients_list = st.multiselect(
